8081d8db0ae7e1592a8e0f34abeb85fd2c796780
emarekica <emarekica@gmail.com>
2022-05-30 00:18:17 +0200

    config(html, css, js): drag and drop

--- form elements presentation/fep.js
+++ form elements presentation/fep.js
@@ -1,21 +1,68 @@
-"use stric";
+"use strict";
 
 // drag 'n' drop
-var fileInput = document.querySelector("input[type=file]");
-var filenameContainer = document.querySelector("#filename");
-var dropzone = document.querySelector("div");
 
-fileInput.addEventListener("change", function () {
-  filenameContainer.innerText = fileInput.value.split("\\").pop();
+const drop = document.querySelector(".drop");
+const input = document.querySelector(".drop input");
+const text = document.querySelector(".text");
+const progress = document.querySelector(".progress");
+
+let files = [];
+
+// Upload Logic
+function upload() {
+  // fake Upload Logic
+  let intervalCount = 0.25;
+  progress.style.display = "block";
+  progress.style.width = `${20 * intervalCount}%`;
+  const interval = setInterval(() => {
+    intervalCount += 0.25;
+    progress.style.width = `${20 * intervalCount}%`;
+    if (intervalCount == 5) {
+      clearInterval(interval);
+    }
+  }, 100);
+}
+
+input.addEventListener("change", () => {
+  drop.style.display = "none";
+  upload();
 });
 
-fileInput.addEventListener("dragenter", function () {
-  dropzone.classList.add("dragover");
+drop.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  text.innerHTML = "Release your mouse to drop.";
+  drop.classList.add("active");
 });
 
-fileInput.addEventListener("dragleave", function () {
-  dropzone.classList.remove("dragover");
+drop.addEventListener("dragleave", (e) => {
+  e.preventDefault();
+  text.innerHTML = "Drag and drop your documents, photos, and video here.";
+  drop.classList.remove("active");
 });
+
+drop.addEventListener("drop", (e) => {
+  e.preventDefault();
+  files = e.dataTransfer.files;
+  drop.style.display = "none";
+  upload();
+});
+
+// let fileInput = document.querySelector("input[type='file']");
+// let filenameContainer = document.getElementById("#filename");
+// let dropzone = document.querySelector(".dropzone");
+
+// fileInput.addEventListener("change", function () {
+//   filenameContainer.innerText = fileInput.value.split("\\").pop();
+// });
+
+// fileInput.addEventListener("dragenter", function () {
+//   dropzone.classList.add("dragover");
+// });
+
+// fileInput.addEventListener("dragleave", function () {
+//   dropzone.classList.remove("dragover");
+// });
 
 // ----
 // use sessionStorage
